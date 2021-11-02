5a056ac4139b9a62784bf1180792291f7f76bd34
Eric Cano <eric.cano@cern.ch>
2021-11-02 14:28:24 +0100

    Removed duplicate information.

--- HeterogeneousCore/MPICore/test/testMPImultihost.cc
+++ HeterogeneousCore/MPICore/test/testMPImultihost.cc
@@ -20,15 +20,8 @@ int main(int argc, char** argv) {
   int name_len;
   MPI_Get_processor_name(processor_name, &name_len);
 
-  // Print off a hello world message
-  char hostname[HOST_NAME_MAX + 1];
-  auto rc = gethostname(hostname, sizeof(hostname));
-  if (rc) {
-    perror("Failed to get hostname: ");
-    hostname[0] = '\0';
-  }
-  printf("Hello world from processor %s, rank %d out of %d processors on host %s\n",
-         processor_name, world_rank, world_size, hostname);
+  printf("Hello world from processor %s, rank %d out of %d processors\n",
+         processor_name, world_rank, world_size);
 
   // Finalize the MPI environment.
   MPI_Finalize();
