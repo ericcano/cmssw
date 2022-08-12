c46b4c81ced25083889215cb8e00e7d566025abf
Eric Cano <eric.cano@cern.ch>
2022-08-12 17:25:24 +0200

    Add scoped based range annotation for CUDA tools.

---
+++ HeterogeneousCore/CUDAUtilities/interface/ScopedNVTXRange.h
@@ -0,0 +1,14 @@
+#ifndef HeterogeneousCore_CUDAUtilities_interface_ScopedNVTXRange_h
+
+#include <nvToolsExt.h>
+#include <string>
+
+class ScopedNVTXRange {
+public:
+  ScopedNVTXRange(const std::string & label): nvtxRange_(nvtxRangeStartA(label.c_str())) {}
+  ~ScopedNVTXRange() { nvtxRangeEnd(nvtxRange_); }
+private:
+  nvtxRangeId_t nvtxRange_;
+};
+
+#endif 
