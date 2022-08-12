8d4ef18695cd1e860056f1fd10f17e6064ea9afa
Eric Cano <eric.cano@cern.ch>
2022-08-12 17:25:24 +0200

    Add scoped based range annotation for CUDA tools.

---
+++ HeterogeneousCore/CUDAUtilities/test/ScopedNVTXRange_t.cpp
@@ -0,0 +1,10 @@
+#include "HeterogeneousCore/CUDAUtilities/interface/ScopedNVTXRange.h"
+
+// Validation with:
+// nsys profile ../test/el8_amd64_gcc10/ScopedNVTXRange_t
+// nsys stats report<n>.nsys-rep
+
+int main (void) {
+  ScopedNVTXRange sr1("ScopedRangeTest1");
+  ScopedNVTXRange sr2("ScopedRangeTest2");
+}
