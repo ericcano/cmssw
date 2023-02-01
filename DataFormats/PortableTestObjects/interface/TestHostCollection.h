4f2f713da21d24ff73a0eff5efe0fa19b8dd15f9
Eric Cano <Eri.Cano@cern.ch>
2023-02-01 17:22:48 +0100

    [wip] Adding shared classes declarations for dictionary to DataFormats/Portable

--- DataFormats/PortableTestObjects/interface/TestHostCollection.h
+++ DataFormats/PortableTestObjects/interface/TestHostCollection.h
@@ -19,14 +19,6 @@ namespace ROOT {
   namespace Meta {
     namespace Selection {
       namespace portablecollection {
-        template <std::size_t Idx, typename T, typename... Args> 
-        // This is enough to cover all the cases of CollectionImpl which has no members...
-        class CollectionImpl: KeepFirstTemplateArguments<0> {};
-        class c0: CollectionImpl<0, void> {};
-
-        template <std::size_t Idx, typename T>
-        class CollectionLeaf {};
-
         class CL0_TestHostCollection: CollectionLeaf<0, portabletest::TestSoALayout<128,false>> {};
         class CL1_TestHostMultiCollection: CollectionLeaf<1, portabletest::TestSoALayout2<128,false>> {};
       }
