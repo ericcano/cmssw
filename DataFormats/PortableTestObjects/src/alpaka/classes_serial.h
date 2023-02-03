b557faa63c3f400d4f6ab538388a0d0862fd1889
Eric Cano <eric.cano@cern.ch>
2023-02-03 18:16:12 +0100

    [wip] Simplify namespace declaration.

--- DataFormats/PortableTestObjects/src/alpaka/classes_serial.h
+++ DataFormats/PortableTestObjects/src/alpaka/classes_serial.h
@@ -4,27 +4,15 @@
 #include "DataFormats/PortableTestObjects/interface/TestSoA.h"
 #include "DataFormats/Portable/interface/PortableCollectionCommon.h"
 
-namespace ROOT {
-  namespace Meta {
-    namespace Selection {
-      namespace portablecollection {
-        class CL0_TestHostCollection : CollectionLeaf<0, portabletest::TestSoALayout<128, false>> {};
-        class CL1_TestHostMultiCollection : CollectionLeaf<1, portabletest::TestSoALayout2<128, false>> {};
-      }  // namespace portablecollection
-      namespace portabletest {
-        class TestHostCollection : KeepFirstTemplateArguments<1> {};
-        class TestHostMultiCollection : KeepFirstTemplateArguments<2> {};
-      }  // namespace portabletest
-    }    // namespace Selection
-  }      // namespace Meta
-}  // namespace ROOT
+namespace ROOT::Meta::Selection {
+  namespace portablecollection {
+    class c0 : CollectionImpl<0, void> {};
 
-namespace ROOT {
-  namespace Meta {
-    namespace Selection {
-      namespace portablecollection {
-        class c0 : CollectionImpl<0, void> {};
-      }  // namespace portablecollection
-    }    // namespace Selection
-  }      // namespace Meta
-}  // namespace ROOT
+    class CL0_TestHostCollection : CollectionLeaf<0, portabletest::TestSoALayout<128, false>> {};
+    class CL1_TestHostMultiCollection : CollectionLeaf<1, portabletest::TestSoALayout2<128, false>> {};
+  }  // namespace portablecollection
+  namespace portabletest {
+    class TestHostCollection : KeepFirstTemplateArguments<1> {};
+    class TestHostMultiCollection : KeepFirstTemplateArguments<2> {};
+  }  // namespace portabletest
+}    // namespace Selection
