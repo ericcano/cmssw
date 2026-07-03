88993e659dd858160a69a24729713860b4acc3b5
Eric Cano <Eri.Cano@cern.ch>
2026-07-03 15:55:43 +0200

    [WIP/Copilot] Adds debugging with stack traces, fixes wrong tagging in HDF5ProductResolve(?)

---
+++ FWCore/Services/interface/Backtrace.h
@@ -0,0 +1,40 @@
+#ifndef FWCore_Services_Backtrace_h
+#define FWCore_Services_Backtrace_h
+
+#include <pthread.h>
+#include <string>
+
+namespace cta::exception {
+
+class Backtrace {
+public:
+  explicit Backtrace(bool fake = false);
+
+  const std::string& str() const { return m_trace; }
+
+private:
+  std::string m_trace;
+
+  /**
+   * Singleton lock around the apparently racy backtrace().
+   * We write it with no error check as it's used only here.
+   * We need a class in order to have a constructor for the global object.
+   */
+  class mutex {
+  public:
+    mutex() { pthread_mutex_init(&m_mutex, nullptr); }
+
+    void lock() { pthread_mutex_lock(&m_mutex); }
+
+    void unlock() { pthread_mutex_unlock(&m_mutex); }
+
+  private:
+    pthread_mutex_t m_mutex;
+  };
+
+  static mutex g_lock;
+};
+
+}  // namespace cta::exception
+
+#endif // FWCore_Services_Backtrace_h
